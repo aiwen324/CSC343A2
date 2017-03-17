d039d74da4214a2662ca32d9f55b3c85713fb406
aiwen324 <aiwen324@gmail.com>
2017-03-17 18:58:04 -0400

    finsih q5, start debug

--- part1-files/q5.sql
+++ part1-files/q5.sql
@@ -12,10 +12,33 @@ CREATE TABLE q5 (
 
 -- You may find it convenient to do this for each of the views
 -- that define your intermediate steps.  (But give them better names!)
-DROP VIEW IF EXISTS intermediate_step CASCADE;
+DROP VIEW IF EXISTS grader_with_aid_num CASCADE;
 
 -- Define views for your intermediate steps here.
 
+-- View for 
+CREATE VIEW grader_with_aid_num AS
+	select assignment_id, username, count(group_id) as num_assigned
+	from Grader natural join AssignmentGroup
+	group by assignment_id, username;
+
+-- Find the ast id that are out of range
+CREATE VIEW gid_aid_num AS
+	select assignment_id
+	from grader_with_aid_num
+	group by assignment_id
+	having max(num_assigned) - min(num_assigned) > 10;
+
+-- View for final result
+CREATE VIEW grader_aid_num_out_of_range AS
+	select assignment_id, username, num_assigned
+	from grader_with_aid_num
+	where assignment_id in (select assignment_id from gid_aid_num);
+
+
+
+
+
 -- Final answer.
-INSERT INTO q5 
+INSERT INTO q5 (select * from grader_aid_num_out_of_range);
 	-- put a final query here so that its results will go into the table.
