e29331b4543da84a947a63e67196978a4aaf7185
aiwen324 <aiwen324@gmail.com>
2017-03-17 19:36:26 -0400

    finish q6

--- part1-files/q6.sql
+++ part1-files/q6.sql
@@ -17,10 +17,53 @@ CREATE TABLE q6 (
 
 -- You may find it convenient to do this for each of the views
 -- that define your intermediate steps.  (But give them better names!)
-DROP VIEW IF EXISTS intermediate_step CASCADE;
+DROP VIEW IF EXISTS gid CASCADE;
 
 -- Define views for your intermediate steps here.
+-- View for group_id for A1
+CREATE VIEW gid AS
+	select group_id
+	from AssignmentGroup natural join Assignment
+	where description = 'A1';
+
+-- View for gid with submission, need every group appear,
+-- So we will use the natural join
+CREATE VIEW a1group_submission AS
+	select group_id, submission_id, file_name,
+	username, submission_date
+	from gid natural left join submissions;
+
+-- View for first submit for each group
+CREATE VIEW group_submission_first AS
+	select group_id, min(submission_date) as submission_date
+	from a1group_submission
+	group by group_id;
+
+-- View for last submit for each group
+CREATE VIEW group_submission_last AS
+	select group_id, max(submission_date) as submission_date
+	from a1group_submission
+	group by group_id;
+
+-- View for View for first submit for each group with details
+CREATE VIEW group_submission_first_details AS
+	select group_id, file_name as first_file, submission_date
+	as first_time, username as first_submitter
+	from a1group_submission natural join group_submission_first;
+
+CREATE VIEW group_submission_last_details AS
+	select group_id, file_name as last_file, submission_date
+	as last_time, username as last_submitter
+	from a1group_submission natural join group_submission_last;
+
+CREATE VIEW group_submission_details AS
+	select group_id, first_file, first_time, first_submitter,
+	last_file, last_time, last_submitter, (last_time - first_time)
+	as elapsed_time
+	from group_submission_first_details natural join 
+	group_submission_last_details;
+
 
 -- Final answer.
-INSERT INTO q6 
+INSERT INTO q6 (select * from group_submission_details);
 	-- put a final query here so that its results will go into the table.
